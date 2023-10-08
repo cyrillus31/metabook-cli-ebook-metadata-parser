f3dfe8caf5b0fc66a2e14e34a103a86c8c804a71
Kirill F <kirill.olegovich31@gmail.com>
2023-10-09 01:37:33 +0300

    added README

--- Task_2/metabook/ebook/epub.py
+++ Task_2/metabook/ebook/epub.py
@@ -52,7 +52,6 @@ class EPUB_book(Ebook):
 
         tree = ET.parse(opf_path)
         root = tree.getroot()
-        # print(root.tag)
         metadata_xml = root.find(f"{self.namespace}metadata")
 
         if not metadata_xml:
